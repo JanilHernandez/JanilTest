e18bfb30d8f0697f433284deb05913b6c65c7af6
nandohdev <janil.quimica@gmail.com>
2023-06-01 18:20:54 -0300

    updates

---
+++ guia 7/ejercicio15guia7/src/ejercicio15guia7/Ejercicio15guiaJAVA.java
@@ -0,0 +1,23 @@
+/*
+Realizar un algoritmo que rellene un vector con los 100 primeros números enteros y los
+muestre por pantalla en orden descendente.
+ */
+package ejercicio15guia7;
+import java.util.Scanner;
+public class Ejercicio15guiaJAVA {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int[] Vector = new int[101];
+        
+        llenarVector(Vector);
+    }
+     public static void llenarVector(int[] Vector) {
+        int llenado;
+        
+        for (llenado = 0; llenado <= 100; llenado--){
+            Vector[llenado] = llenado;
+        }
+            System.out.print(llenado);
+     }
+}
